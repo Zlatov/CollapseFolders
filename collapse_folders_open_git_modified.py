fe9be4ce1b82d6a5efc42044e352c4141ea91db0
zlatov <zlatov@ya.ru>
2019-11-08 20:22:11 +0300

    q

--- collapse_folders_open_git_modified.py
+++ collapse_folders_open_git_modified.py
@@ -3,6 +3,7 @@ import pprint, subprocess, re
 from pprint import pprint
 import os
 from subprocess import check_output
+import json
 
 class CollapseFoldersOpenGitModifiedCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -33,13 +34,22 @@ class CollapseFoldersOpenGitModifiedNewCommand(sublime_plugin.TextCommand):
     view = self.view # вьюха открытого файла?
     window = view.window() # экземпляр окна сублайма?
 
+    # Путь к файлу проекта
+    # файл может находиться не в проекте, поэтому
+    # парсим json в файле и смотрим первую попавшуюся папку
     project_path = window.project_file_name()
+    if project_path != None:
+      project_file = open(project_path)
+      json_project = project_file.read()
+      dict_project = json.loads(json_project)
+      if dict_project is not None and 'folders' in dict_project and dict_project['folders'][0] is not None:
+        project_path = os.path.join(os.path.dirname(project_path), dict_project['folders'][0]['path'])
+        # print('project_path:', project_path)
+        # print('os.path.isdir(project_path):', os.path.isdir(project_path))
     if project_path == None:
       project_path = window.folders()[0]
-    else:
-      project_path = os.path.dirname(project_path)
-
-    pprint(project_path)
+    # else:
+      # project_path = os.path.dirname(project_path)
 
     changed_files = check_output(
       [
